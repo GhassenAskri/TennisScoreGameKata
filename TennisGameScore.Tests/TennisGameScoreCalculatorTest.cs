54318d1bdc6f701e97373bd69c9f02a7b2a38016
Ghassen ASKRI <askrighassem@gmail.com>
2021-10-03 18:29:17 +0200

    Refactor First Test

--- TennisGameScore.Tests/TennisGameScoreCalculatorTest.cs
+++ TennisGameScore.Tests/TennisGameScoreCalculatorTest.cs
@@ -6,12 +6,13 @@ namespace TennisGameScore.Tests
 {
     public class TennisGameScoreCalculatorTest
     {
+        private readonly string _welcomeMessage =  "Wellcome to the tennis game";
         [Fact]
         public void WHEN_THE_TENNIS_SCORE_GAME_BEGIN_WELCOME_MESSAGE_SHOULD_BE_PROMPED()
         {
             TennisGameScoreCalculator tennisGameScoreCalculator = new TennisGameScoreCalculator();
             string tennisGameScoreCalculatorWelcomeMessage = tennisGameScoreCalculator.BeginGame();
-            Assert.Equal("Wellcome to the tennis game",tennisGameScoreCalculatorWelcomeMessage);
+            Assert.Equal(_welcomeMessage,tennisGameScoreCalculatorWelcomeMessage);
         }
     }
 }
