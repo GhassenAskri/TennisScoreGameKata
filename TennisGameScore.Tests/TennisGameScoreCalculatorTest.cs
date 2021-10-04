4e32a0fa9cd5f5267876caf8997f5301be39ce85
Ghassen ASKRI <askrighassem@gmail.com>
2021-10-04 23:08:58 +0200

    Refactor Test

--- TennisGameScore.Tests/TennisGameScoreCalculatorTest.cs
+++ TennisGameScore.Tests/TennisGameScoreCalculatorTest.cs
@@ -7,7 +7,15 @@ namespace TennisGameScore.Tests
 {
     public class TennisGameScoreCalculatorTest
     {
+        #region Setup
         private readonly string _welcomeMessage =  "Welcome to the tennis score game calculator";
+        private  TennisPlayer _firstPlayer = new TennisPlayer("Rafeal Nadal");
+        private TennisPlayer _secondPlayer = new TennisPlayer("Roger Federer");
+        private Dictionary<string,string> _gameScore;
+        private TennisGameScoreCalculator _tennisGameScoreCalculator; 
+        #endregion
+
+        #region Tests
         [Fact]
         public void When_The_Tennis_Score_Game_Calculator_Called_Welcome_Message_Should_Be_Returned()
         {
@@ -19,22 +27,14 @@ namespace TennisGameScore.Tests
         public void When_The_First_Player_Win_The_First_Ball()
         {
             //Given
-            TennisPlayer firstPlayer = new TennisPlayer("Rafeal Nadal");
-            TennisPlayer secondPlayer = new TennisPlayer("Roger Federer");
-            Dictionary<string,string> gameScore;
-
+            _firstPlayer.HasWinAPoint("love"); 
             //When
-            firstPlayer.HasWinAPoint("love");
-            
-            //Then
-            TennisGameScoreCalculator tennisGameScoreCalculator = new TennisGameScoreCalculator();
-            
-            gameScore = tennisGameScoreCalculator.CalculateScore(firstPlayer,secondPlayer);
-            string firstPlayerScore = gameScore[firstPlayer.Name];
+            _tennisGameScoreCalculator = new TennisGameScoreCalculator();
+            _gameScore = _tennisGameScoreCalculator.CalculateScore(_firstPlayer,_secondPlayer);
+            string firstPlayerScore = _gameScore[_firstPlayer.Name]; 
             //Assert
             Assert.True(firstPlayerScore == "love");
         }
-
-        
+        #endregion
     }
 }
