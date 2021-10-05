6155f66e60733840ec8e910bfc09e98860c187bd
Ghassen ASKRI <askrighassem@gmail.com>
2021-10-06 00:59:59 +0200

    Refactor code

--- TennisGameScore/Winner.cs
+++ TennisGameScore/Winner.cs
@@ -4,9 +4,10 @@ namespace TennisGameScore
 {
     public static class Winner 
     {
+        private static string _gameBallPoint = "winner";
         public static string GetTheWinnerName(Player firstPlayer, Player secondPlayer)
         {
-            if(secondPlayer.Score.Contains("winner"))
+            if(secondPlayer.Score.Contains(_gameBallPoint))
                 return secondPlayer.Name;
             return firstPlayer.Name;
         }
