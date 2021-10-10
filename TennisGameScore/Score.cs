be009f17a5108bcd0040a15ff3cd94c54733b2f8
Ghassen ASKRI <askrighassem@gmail.com>
2021-10-10 18:13:58 +0200

    Test succeed

--- TennisGameScore/Score.cs
+++ TennisGameScore/Score.cs
@@ -7,21 +7,27 @@ namespace TennisGameScore
 {
     public class Score
     {
+        #region Constructor
         public Score()
         {
             this.ScoreRecordPoints = new Collection<string>();
         }
+        #endregion
+        #region Properties
         private Dictionary<object,string> _scoreDictionaryRewards = new Dictionary<object, string>(){
             {1,"love"},
             {2,"fifteen"},
             {3,"thirteen"},
             {4,"fourteen"},
+            {5,"advantage"},
             {"gameBall","winner"},
         };
         public ICollection<string> ScoreRecordPoints { get; set;}
-        
+        #endregion
+        #region Methods 
         public void AddNewScorePoint(object _numberOfSucceffulBalls)
         {
+
             this.ScoreRecordPoints.Add(_scoreDictionaryRewards[_numberOfSucceffulBalls]);
         
         }
@@ -31,6 +37,6 @@ namespace TennisGameScore
             return String.Join(_delimiter,ScoreRecordPoints);
         }
     }
-
+    #endregion
    
 }
